d8ade25bb4f4473b330789fa458b902930cafe3a
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2021-01-30 21:57:28 -0500

    added in/out dialogboc animation

--- Sock Noir/Assets/DialogManager.cs
+++ Sock Noir/Assets/DialogManager.cs
@@ -7,21 +7,28 @@ public class DialogManager : MonoBehaviour
 {
     public Text name;
     public Text dialog;
+    public Animator animator;
+
     private Queue<string> sentences;
 
-    public void StartDialog(Dialog dialog){
+    public void StartDialog(Dialog dialog)
+    {
+        animator.SetBool("isOpen", true);
         name.text = dialog.name;
 
         sentences.Clear();
-        foreach (string sentence in dialog.sentences) {
+        foreach (string sentence in dialog.sentences)
+        {
             sentences.Enqueue(sentence);
         }
 
         DisplayNextSentence();
     }
 
-    public void DisplayNextSentence() {
-        if (sentences.Count == 0) {
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
             EndDialog();
             return;
         }
@@ -30,8 +37,9 @@ public class DialogManager : MonoBehaviour
         dialog.text = sentence;
     }
 
-    void EndDialog() {
-        Debug.Log("No more sentences!"); 
+    void EndDialog()
+    {
+        animator.SetBool("isOpen", false);
     }
 
     // Start is called before the first frame update
