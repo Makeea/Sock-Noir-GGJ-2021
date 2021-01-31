69d28e6c935533f17cdce4b6995ba1b553bf23a2
Cian Callanan <Cian.Callanan@viagogo.com>
2021-01-31 14:52:40 -0500

    Update ChatterBox.cs

--- Sock Noir/Assets/Scripts/ChatterBox.cs
+++ Sock Noir/Assets/Scripts/ChatterBox.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ChatterBox : Interactable
@@ -12,16 +10,11 @@ public class ChatterBox : Interactable
     void Update()
     {
         if (interact && Input.GetButton("Fire1"))
-        {
-            Debug.Log($"HERE");
-            var dialog = new Dialog();
-            // dialog.name = "Dave";
-            // dialog.sentences = new string[] {
-            //     "Oof!",
-            //     "Watch where you're colliding, sir!"
-            // };
-            dialog.name = characterName;
-            dialog.sentences = sentences;
+        {            
+            var dialog = new Dialog{
+                name = characterName,
+                sentences = sentences
+            };
             FindObjectOfType<DialogManager>().StartDialog(dialog);
         }
     }
